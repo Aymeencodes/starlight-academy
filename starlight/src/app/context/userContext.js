6fb5bf886fd02f1cf1194880caf805ed20166e9a
aymen <aymen.code.dev@gmail.com>
2024-04-02 22:28:43 +0100

    lll

--- starlight/src/app/context/userContext.js
+++ starlight/src/app/context/userContext.js
@@ -4,8 +4,9 @@ import { createContext,useEffect,useState } from "react";
 export const UserContext = createContext()
 
 const UserProvider = ({children})=>{
-    const [currentUser, setcurrentUser] = useState('');
+    const [currentUser, setcurrentUser] = useState(JSON.parse(localStorage.getItem('user')) || {});
     useEffect(()=>{
+        localStorage.setItem('user',JSON.stringify(currentUser))
     },[currentUser])
     return <UserContext.Provider value={{currentUser,setcurrentUser}}>{children}</UserContext.Provider>
 }
