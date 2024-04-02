40cbdfd8461bd7680080e497bb4c6ff4e0aa803d
aymen <aymen.code.dev@gmail.com>
2024-04-02 22:07:27 +0100

    tt

--- starlight/src/app/context/userContext.js
+++ starlight/src/app/context/userContext.js
@@ -1,30 +1,13 @@
 "use client"
-import { createContext, useEffect, useState } from "react";
+import { createContext,useEffect,useState } from "react";
 
-export const UserContext = createContext();
+export const UserContext = createContext()
 
-const UserProvider = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState(() => {
-        if (typeof window !== 'undefined') {
-            // Access localStorage only if window is defined (i.e., in the browser)
-            return JSON.parse(localStorage.getItem('user')) || {};
-        } else {
-            return {};
-        }
-    });
+const UserProvider = ({children})=>{
+    const [currentUser, setcurrentUser] = useState('');
+    useEffect(()=>{
+    },[currentUser])
+    return <UserContext.Provider value={{currentUser,setcurrentUser}}>{children}</UserContext.Provider>
+}
 
-    useEffect(() => {
-        if (typeof window !== 'undefined') {
-            // Access localStorage only if window is defined (i.e., in the browser)
-            localStorage.setItem('user', JSON.stringify(currentUser));
-        }
-    }, [currentUser]);
-
-    return (
-        <UserContext.Provider value={{ currentUser, setCurrentUser }}>
-            {children}
-        </UserContext.Provider>
-    );
-};
-
-export default UserProvider;
+export default UserProvider
