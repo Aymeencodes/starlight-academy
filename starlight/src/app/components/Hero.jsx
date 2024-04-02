8c7adc3603875f1f28a648d5ad7b125e4c41abc9
aymen <aymen.code.dev@gmail.com>
2024-04-02 14:59:27 +0100

    tt

--- starlight/src/app/components/Hero.jsx
+++ starlight/src/app/components/Hero.jsx
@@ -77,7 +77,7 @@ function Hero() {
                     ✕
                   </button>
                 </form>
-                <div className=" mt-3 w-[100%]">
+                {/*<div className=" mt-3 w-[100%]">
                   <iframe
                     width="100%"
                     height="600"
@@ -85,10 +85,10 @@ function Hero() {
                     scrolling="no"
                     marginheight="0"
                     marginwidth="0"
-                    src={encodedUrl}
+                    src='https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=1%20Grafton%20Street,%20Dublin,%20Ireland+(starlight)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed'
                   ></iframe>
                   <a href="https://www.gps.ie/">gps systems</a>
-                </div>
+  </div>*/}
               </div>
             </dialog>
             <a
