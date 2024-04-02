49cfc13df2bed0d12eeeda27d43eeefedb35fe0b
aymen <aymen.code.dev@gmail.com>
2024-04-02 22:36:53 +0100

    mmmm

--- starlight/src/app/context/userContext.js
+++ starlight/src/app/context/userContext.js
@@ -1,14 +1,28 @@
 "use client"
-import { createContext,useEffect,useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
-export const UserContext = createContext()
+export const UserContext = createContext();
 
-const UserProvider = ({children})=>{
-    const [currentUser, setcurrentUser] = useState(JSON.parse(localStorage.getItem('user')) || {});
-    useEffect(()=>{
-        localStorage.setItem('user',JSON.stringify(currentUser))
-    },[currentUser])
-    return <UserContext.Provider value={{currentUser,setcurrentUser}}>{children}</UserContext.Provider>
-}
+const UserProvider = ({ children }) => {
+    const [currentUser, setCurrentUser] = useState(() => {
+        if (typeof window !== 'undefined') {
+            return JSON.parse(localStorage.getItem('user')) || {};
+        } else {
+            return {};
+        }
+    });
 
-export default UserProvider
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            localStorage.setItem('user', JSON.stringify(currentUser));
+        }
+    }, [currentUser]);
+
+    return (
+        <UserContext.Provider value={{ currentUser, setCurrentUser }}>
+            {children}
+        </UserContext.Provider>
+    );
+};
+
+export default UserProvider;
