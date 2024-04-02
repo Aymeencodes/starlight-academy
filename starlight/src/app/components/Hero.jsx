64f0393103eceed11b9956e20f0f48098406db27
aymen <aymen.code.dev@gmail.com>
2024-04-02 14:43:40 +0100

    v

--- starlight/src/app/components/Hero.jsx
+++ starlight/src/app/components/Hero.jsx
@@ -83,7 +83,7 @@ function Hero() {
                     scrolling="no"
                     marginheight="0"
                     marginwidth="0"
-                    src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=1%20Grafton%20Street,%20Dublin,%20Ireland+(starlight)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+                    src='https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=1%20Grafton%20Street,%20Dublin,%20Ireland+(starlight)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed'
                   ></iframe>
                   <a href="https://www.gps.ie/">gps systems</a>
                 </div>
